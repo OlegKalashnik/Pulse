252e3d091d9bcfbd5f2404e95ed3ebab979d2b12
Oleg Kalashnik <olegkalashnik@MacBook-Air-Oleg.local>
2022-06-13 01:08:20 +0300

    modals

--- src/js/script.js
+++ src/js/script.js
@@ -49,4 +49,18 @@ $(document).ready(function () {
   }
   toggleSlide('.catalog-item__link')
   toggleSlide('.catalog-item__link__back')
+
+  //modals
+  $('[data-modal=consultation]').on('click', function () {
+    $('.overlay, #consultation').fadeIn('slow')
+  })
+  $('.modal__close').on('click', function () {
+    $('.overlay, #consultation, #order, #thanks').fadeOut('slow')
+  })
+  $('[data-modal=order]').each(function (i) {
+    $(this).on('click', function () {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text())
+      $('.overlay, #order').fadeIn('slow')
+    })
+  })
 })
